e85083cc243e1d50bebdb95cdc7e91f7c9205768
Paulbrs <pavel_balaba05@mail.ru>
2025-11-20 01:06:58 +0300

    Revert "fix"
    This reverts commit 11e0a223d90f65523b3a6f21398f30807c6209c4.

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,21 +1,21 @@
 import { prisma } from "@/prisma/prisma-client";
 import { authOptions } from "@/shared/constants/auth-options";
 import { getServerSession } from "next-auth";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = 'force-dynamic';
 
-export async function GET(req: any, res: any) {
+export async function GET(req: NextRequest) {
     try {
-        const user = await getServerSession(req, res, authOptions);
+        const session = await getServerSession(authOptions);
 
-        if(!user) {
+        if(!session?.user) {
             return NextResponse.json({ message: 'Вы не авторизованы!!' }, { status: 401 });
         }
 
         const data = await prisma.user.findUnique({
             where: {
-                id: Number(user.user.id),
+                id: Number(session.user.id),
             },
             select: {
                 fullName: true,
